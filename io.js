da6e6b1c5875f408091082eb979a2a745703fed2
Sangura Sitati <sangura.sitati@gmail.com>
2015-02-18 16:16:26 +0300

    io.js - add command line arguments

--- io.js
+++ io.js
@@ -1,6 +1,6 @@
 // filesystem operations need the fs module from the Node core library
 var fs = require('fs');
-var buf = fs.readFileSync('hipsum.txt');
+var buf = fs.readFileSync(process.argv[2]);
 var str = buf.toString();
 var lines = str.split('\n');
 console.log(lines.length - 1);
