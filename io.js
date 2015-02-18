e358dbe7567c221308d3f7f718adea9717333833
Sangura Sitati <sangura.sitati@gmail.com>
2015-02-18 16:35:43 +0300

    io.js - add comments

--- io.js
+++ io.js
@@ -1,6 +1,10 @@
 // filesystem operations need the fs module from the Node core library
 var fs = require('fs');
+//read file as a command line argument
 var buf = fs.readFileSync(process.argv[2]);
+//convert buffer to string
 var str = buf.toString();
+//split string at newline characters
 var lines = str.split('\n');
+// print number of lines
 console.log(lines.length - 1);
