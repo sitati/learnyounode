dc22f8708cfd81eb1c6b8d3ba3cdb3f90a0f19a3
Sangura Sitati <sangura.sitati@gmail.com>
2015-02-18 12:48:57 +0300

    add comments sum.js

--- sum.js
+++ sum.js
@@ -1,4 +1,8 @@
 var sum = 0;
+// The first element of the process.argv array is always 'node', 
+//and the second element is always the path to your program.js file, 
+//so you need to start at the 3rd element (index 2)
+
 for(i = 2; i < process.argv.length; i++){
     sum += +process.argv[i];
 }
