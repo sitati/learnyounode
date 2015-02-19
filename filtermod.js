c6cd74abfe50b688320a36a1d33f2af06f08db48
Sangura Sitati <sangura.sitati@gmail.com>
2015-02-19 17:12:54 +0300

    filter_ls_mod.js
    filter_ls_mod.js filtermod.js - add make it modular solution

---
+++ filtermod.js
@@ -0,0 +1,12 @@
+var filter_ls_mod = require('./filter_ls_mod');
+var dir = process.argv[2];
+var ext = process.argv[3];
+
+filter_ls_mod(dir, ext, function (err, list){
+   if(err)
+       return console.error('There was an error', err);
+   
+   list.forEach(function (file){
+       console.log(file)
+   });
+});
