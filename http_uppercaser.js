3dad58056c2182f25ed79b7d7f08d2e4e78196a6
Sangura Sitati <sangura.sitati@gmail.com>
2015-02-24 17:08:35 +0300

    modify http_uppercaser.js - remove unecessary lines

--- http_uppercaser.js
+++ http_uppercaser.js
@@ -3,7 +3,6 @@ var fs = require('fs');
 var map = require('through2-map');
 
 var port = process.argv[2];
-var file = process.argv[3];
 
 var server = http.createServer(function(request, response) {
     if (request.method !== 'POST')
